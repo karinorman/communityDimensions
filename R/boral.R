963c1b45399b0560548ea8285cec3429c97808f3
sastoudt <sstoudt@berkeley.edu>
2019-05-28 12:23:37 -0700

    add information about how bryophytes data is structured

--- R/boral.R
+++ R/boral.R
@@ -1,9 +1,32 @@
 detach("package:boral",unload=T) ## get rid of old boral if you have it
 library(here)
 
+#### data ####
 load(here::here("data", "bryophytes.rda"))
 ## source file originally from: https://github.com/guiblanchet/HMSC/blob/master/data/bryophytes.rda
 
+names(bryophytes)
+## Y: binary, occurrence
+dim(bryophytes$Y) ## 204 (sites) x 60 (species)
+
+## X: covariates
+dim(bryophytes$X) ## 204 (sites) x 5 (number of covariates)
+colnames(bryophytes$X)
+
+## Tr: traits
+dim(bryophytes$Tr) ## 5 (traits) x 60 (species)
+row.names(bryophytes$Tr)
+
+## Phylo: phylogenetic relationships between species
+dim(bryophytes$Phylo) ## 60 (species) x 60 (species)
+
+## Random: structure for random effects
+dim(bryophytes$Random) ## 204 (sites) x 2
+length(unique(bryophytes$Random[, 1])) ## 204 (trees)
+length(unique(bryophytes$Random[, 2])) ## 28 (sites)
+
+
+#### subsetting ####
 J <- 30 ## downsample to make things faster
 N <- 150
 
@@ -15,6 +38,7 @@ newY = newY[toKeepN,] ## occur/no occur
 newX= bryophytes$X[toKeepN,] ## covariates, first column is an intercept so we remove this later on
 ## boral adds an intercept automatically
 
+#### model ####
 library(devtools)
 install_github("sastoudt/boral",  ref = "ppc") ## my version that gives you posterior predictive information
 require(boral)
