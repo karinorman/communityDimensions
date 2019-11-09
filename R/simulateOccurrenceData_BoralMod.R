09ec9eb6de4490486be94736da772035a923e7e6
sastoudt <sstoudt@berkeley.edu>
2019-11-09 10:02:40 -0800

    update matrix simulation to better reflect misspecification we want, based on Will feedback

--- R/simulateOccurrenceData_BoralMod.R
+++ R/simulateOccurrenceData_BoralMod.R
@@ -9,6 +9,11 @@ library(MASS)
 ## signal: magnitude of a non-zero entry (stick to stuff between 0 and 1)
 ## d: size of matrix
 ## this chunk modified from code originally from Will
+
+## this matrix generation might not be what we want because a matrix can be
+## dense but still low rank (well approximated by a small number of latent factors)
+
+## from Will: any matrix can be approximated by a latent factor model with enough latent factors, the question is whether it can be approximated by a few latent factors (like the low rank assumption makes)
 helperCreateMatrixNice <- function(sparsity, signal, d) {
   precision.mat <- diag(d)
   precision.mat[upper.tri(precision.mat)] <- signal * (runif(d * (d - 1) / 2) < sparsity)
@@ -28,67 +33,39 @@ helperCreateMatrixNice <- function(sparsity, signal, d) {
   }
 }
 
+
+## instead this function generates matrices with certain number of latent factors
+## strength represents how large the covariances can be (larger means stronger relationships)
+getMat <- function(numFactors, strength) {
+  vectors <- lapply(1:numFactors, function(x, y) {
+    runif(30, 0, y)
+  }, strength)
+
+  cov <- lapply(vectors, function(x) {
+    x %*% t(x)
+  })
+
+  A <- matrix(0, nrow = 30, ncol = 30)
+
+  for (i in 1:numFactors) {
+    A <- A + cov[[i]]
+  }
+  return(A)
+}
 #### simulate reasonable latent factors given a plausible species covariance matrix ####
 
 
 ## WARNING: I need to run this part by Will and Perry to make sure it makes sense.
 
+
+
 set.seed(13211)
 
 numSpecies <- 30
 numFactors <- 2
+strength <- 1
 
-weakCorr <- helperCreateMatrixNice(0.1, 0.25, numSpecies)
-## supposed to approximate weak correlations, still not sure if this is doing what I expect
-
-## this chunk modified from code originally from Will
-Sigma <- solve(weakCorr)
-eig <- eigen(weakCorr)
-d <- numSpecies
-n <- 1000
-Z <- matrix(rnorm(n * d), n)
-X <- Z %*% diag(1 / sqrt(eig$values)) %*% t(eig$vectors) ## generate data from that covariance matrix
-
-weakCorrF <- factanal(X, factors = numFactors) ## approximate the data with the given covariance matrix with specified number of factors
-
-weakCorrF$loadings ## now treat these as the true latent factors
-
-set.seed(21213)
-
-strongCorr <- helperCreateNice(0.9, 1, numSpecies)
-## supposed to approximate strong correlations, still not sure if this is doing what I expect
-
-
-Sigma <- solve(strongCorr)
-eig <- eigen(strongCorr)
-Z <- matrix(rnorm(n * d), n)
-X <- Z %*% diag(1 / sqrt(eig$values)) %*% t(eig$vectors)
-
-
-strongCorrF <- factanal(X, factors = numFactors)
-strongCorrF$loadings
-
-#### covariance implied by latent factors - now treated as ground truth ####
-
-weakR <- weakCorrF$loadings %*% t(weakCorrF$loadings) ## covariance implied by latent factors
-
-weakRadj <- nearPD(weakR) ## Not always invertible, so massage
-## annoying thing is now you need weakRadj$mat to get the actual matrix
-
-summary(c(weakR) - c(as.matrix(weakRadj$mat))) ## make sure not too far off
-summary(c(as.matrix(weakRadj$mat)))
-
-strongR <- strongCorrF$loadings %*% t(strongCorrF$loadings)
-strongRadj <- nearPD(strongR)
-
-summary(c(strongR) - c(as.matrix(strongRadj$mat)))
-summary(c(as.matrix(strongRadj$mat)))
-
-weakRadjCorr <- cov2cor(weakRadj) ## switch to correlation to remove anything weird that could be going on with the magnitude, might not be necessary
-strongRadjCorr <- cov2cor(strongRadj)
-
-weakRadjCorrPrec <- solve(weakRadjCorr) ## precision matrix version if you need it at some point
-strongRadjCorrPrec <- solve(strongRadjCorr)
+testMat <- getMat(numFactors, strength)
 
 
 #### given a matrix, simulate occurrence data ####
@@ -115,6 +92,5 @@ simData <- function(mat, numCov, numSites, numSpecies) {
   return(list(Y = sim_y, X = X, X.coefs = X.coefs))
 }
 
-test1 <- simData(weakRadj$mat, numCov, numSites, numSpecies)
+test1 <- simData(testMat, numCov, numSites, numSpecies)
 names(test1)
-test2 <- simData(strongRadj$mat, numCov, numSites, numSpecies)
