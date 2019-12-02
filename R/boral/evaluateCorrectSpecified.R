0e908e05559f44361c1bfee6eec97870932124e8
sastoudt <sstoudt@berkeley.edu>
2019-12-02 11:58:49 -0800

    set up more evaluations

--- R/boral/evaluateCorrectSpecified.R
+++ R/boral/evaluateCorrectSpecified.R
@@ -39,3 +39,41 @@ require(ggplot2)
 
 ggplot(results, aes(numSpecies, kl,col=as.factor(numFactors)))+geom_point()
 ggplot(results, aes(numSpecies, frob,col=as.factor(numFactors)))+geom_point()
+
+
+### misspecified ####
+
+kl <- c()
+frob <- c()
+for(i in 19:nrow(scenarios)){
+  
+  
+  load(file=paste0("correctlySpecifiedResults/r",i,".RData"))
+  
+  
+  estimatedMat = fit.boral$lv.coefs.mean[,-1]%*%t(fit.boral$lv.coefs.mean[,-1])
+  
+  ## put lambda lambda together
+  
+  
+  ## kl divergence
+  
+  kl <- c(kl,klDivergence(trueMats[[i]]+diag(nrow(trueMats[[i]])), estimatedMat+diag(nrow(trueMats[[i]]))))
+  
+  ## frobenius
+  
+  frob <- c(frob,frobeniusNorm(estimatedMat, trueMats[[i]]))
+  
+}
+
+results = cbind.data.frame(scenarios[19:nrow(scenarios),], kl, frob)
+names(results)[1:2]= c("numSpecies", "numFactors")
+
+require(ggplot2)
+
+ggplot(results, aes(numSpecies, kl,col=as.factor(numFactors)))+geom_point()
+ggplot(results, aes(numSpecies, frob,col=as.factor(numFactors)))+geom_point()
+
+## compare magnitudes to other ones
+
+## this needs to go in markdown on github
